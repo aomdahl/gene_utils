52e4e3bd4a945bb4402af3e1a89b2aaf30a6e94f
Ashton Omdahl <aomdahl@gmail.com>
2019-06-03 16:33:22 -0400

    Added concatenate files with omission
    Useful for iterating through a directory and concatenating files only. $1 is the file to not touch, $2 is the output location.
    To include all files, just make $1 a string not found in any of them (i.e. "&")

--- bash_functions.sh
+++ bash_functions.sh
@@ -18,3 +18,23 @@ function transpose()
         awk '{for (f=1;f<=NF;f++) col[f] = col[f]":"$f} END {for (f=1;f<=NF;f++) print col[f]}' $1 | tr ':' '\t'
 
 }
+
+function catWithOmission()
+{
+    #Assumes you want to do it in the local directory
+    FILES=./*
+    NO_TOUCH="$1"
+    COMM=""
+    for f in $FILES
+    do
+    if [[  $f =~ "$NO_TOUCH" ]]; then
+    continue
+    fi
+    if [[ -f "$f" ]]; then
+    echo "Processing $f file...";
+    COMM="$COMM $f"
+    
+    fi
+    done
+    cat $COMM > $2
+}
